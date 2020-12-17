45b59419f229fd471860a2108268ba28d3975d29
RAJEEV KUMAR <rjkr.au@gmail.com>
2020-12-18 07:24:24 +1100

    add taskmanager.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+// Create a `taskHtml` variable with the result of calling the `createTaskHtml` function,
+//  making sure to pass a value for each parameter.
+// const taskHtml = document.querySelector(".container1");
+// console.log(taskHtml);
+
+// Initialize a new TaskManager with currentId set to 0
+const taskmanager = new TaskManager(0);
+
 // Select the New Task Form
 const newTaskForm = document.querySelector("#newTaskForm");
 
@@ -10,6 +18,7 @@ newTaskForm.addEventListener("submit", (event) => {
   const newTaskNameInput = document.querySelector("#newTaskNameInput");
   const newTaskDescription = document.querySelector("#newTaskDescription");
   const newTaskAssignedTo = document.querySelector("#newTaskAssignedTo");
+  const newStatus = document.querySelector("#newStatus");
   const newTaskDueDate = document.querySelector("#newTaskDueDate");
   const errorMessageTN = document.querySelector("#alertMessageTN");
   const errorMessageTDES = document.querySelector("#alertMessageTDES");
@@ -23,7 +32,18 @@ newTaskForm.addEventListener("submit", (event) => {
   const name = newTaskNameInput.value;
   const description = newTaskDescription.value;
   const assignedTo = newTaskAssignedTo.value;
+  const status = newStatus.value;
   const dueDate = newTaskDueDate.value;
+  // // Add the task to the task manager
+  // TaskManager.addTask(name, description, assignedTo, status, dueDate);
+
+  // Clear the form
+  newTaskNameInput.value = "";
+  newTaskDescription.value = "";
+  newTaskAssignedTo.value = "";
+  newStatus.value = "";
+  newTaskDueDate.value = "";
+
   if (!validFormFieldInput(name)) {
     errorMessageTN.innerHTML = "Invalid Name";
     errorMessageTN.style.display = "block";
@@ -48,7 +68,13 @@ newTaskForm.addEventListener("submit", (event) => {
   } else {
     errorMessageDD.style.display = "none";
   }
+  taskmanager.addTask(name, description, assignedTo, status, dueDate);
+
+  taskmanager.render();
+
+  $("#addPostModal").modal("hide");
 });
+
 function validFormFieldInput(data) {
   return data !== null && data !== "";
 }
@@ -65,6 +91,7 @@ editTaskForm.addEventListener("submit", (event) => {
   const editTaskNameInput = document.querySelector("#editTaskNameInput");
   const editTaskDescription = document.querySelector("#editTaskDescription");
   const editTaskAssignedTo = document.querySelector("#editTaskAssignedTo");
+  // const editStatus = document.querySelector("#editStatus");
   const editTaskDueDate = document.querySelector("#editTaskDueDate");
   const errorMessageEditTN = document.querySelector("#alertMessageEditTN");
   const errorMessageEditTDES = document.querySelector("#alertMessageEditTDES");
@@ -81,7 +108,15 @@ editTaskForm.addEventListener("submit", (event) => {
   const editName = editTaskNameInput.value;
   const editDescription = editTaskDescription.value;
   const editAssignedTo = editTaskAssignedTo.value;
+  // const editStatus = editStatus.value;
   const editDueDate = editTaskDueDate.value;
+
+  // // Clear the form
+  // editTaskNameInput.value = "";
+  // editTaskDescription.value = "";
+  // editTaskAssignedTo.value = "";
+  // editStatus.value = "";
+  // editTaskDueDate.value = "";
 
   if (!validFormFieldInput(editName)) {
     errorMessageEditTN.innerHTML = "Invalid Name";
@@ -111,3 +146,6 @@ editTaskForm.addEventListener("submit", (event) => {
 function validFormFieldInput(data) {
   return data !== null && data !== "";
 }
+
+// const task = new TaskManager();
+// console.log(task.tasks);
