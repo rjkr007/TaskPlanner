dda19ba5db6d393b7882b3750b31e1423b2fe102
RAJEEV KUMAR <rjkr.au@gmail.com>
2021-02-08 12:13:23 +1100

    Add Edit feature

--- js/index.js
+++ js/index.js
@@ -9,6 +9,41 @@ taskManager.render();
 
 // Select the New Task Form
 const newTaskForm = document.querySelector("#newTaskForm");
+const newTaskDueDate = document.querySelector("#newTaskDueDate");
+// console.log(newTaskDueDate + "line 12");
+const newDateToday = new Date();
+// console.log(newDateToday + "line 14");
+
+// Select the title of the task modal so can switch between add & edit modes
+const taskModalTitle = document.querySelector("#createTaskModalLabel");
+const updateTask = document.querySelector("#updateTask");
+// Store taskform mode - add OR edit
+let taskFormMode;
+// Empty variable to store edited task temporarily
+let editedTask;
+
+// Event listener for add task button
+const createTaskButton = document.querySelector("#createTaskButton");
+createTaskButton.addEventListener("click", () => {
+  taskFormMode = "add";
+  taskModalTitle.innerHTML = "Add Task";
+  updateTask.innerHTML = "Add Task";
+});
+
+// Select the inputs for Add Task Form
+const newTaskNameInput = document.querySelector("#newTaskNameInput");
+const newTaskDescription = document.querySelector("#newTaskDescription");
+const newTaskAssignedTo = document.querySelector("#newTaskAssignedTo");
+const newStatus = document.querySelector("#newStatus");
+// const newTaskDueDate = document.querySelector("#newTaskDueDate");
+// const newDateToday = new Date();
+const errorMessageTN = document.querySelector("#alertMessageTN");
+const errorMessageTDES = document.querySelector("#alertMessageTDES");
+const errorMessageASSTO = document.querySelector("#alertMessageASSTO");
+const errorMessageDD = document.querySelector("#alertMessageDD");
+
+// Select task modal window
+let taskModal = document.getElementById("addPostModal");
 
 // Add an 'onsubmit' event listener
 newTaskForm.addEventListener("submit", (event) => {
@@ -16,29 +51,17 @@ newTaskForm.addEventListener("submit", (event) => {
   // Prevent default action
   event.preventDefault();
 
-  // Select the inputs for Add Task Form
-  const newTaskNameInput = document.querySelector("#newTaskNameInput");
-  const newTaskDescription = document.querySelector("#newTaskDescription");
-  const newTaskAssignedTo = document.querySelector("#newTaskAssignedTo");
-  const newStatus = document.querySelector("#newStatus");
-  const newTaskDueDate = document.querySelector("#newTaskDueDate");
-  const newDateToday = new Date();
-  const errorMessageTN = document.querySelector("#alertMessageTN");
-  const errorMessageTDES = document.querySelector("#alertMessageTDES");
-  const errorMessageASSTO = document.querySelector("#alertMessageASSTO");
-  const errorMessageDD = document.querySelector("#alertMessageDD");
-
-  // Validation code here
   // Get the values of the inputs
-  const name = newTaskNameInput.value;
+  const taskName = newTaskNameInput.value;
   const description = newTaskDescription.value;
   const assignedTo = newTaskAssignedTo.value;
   const status = newStatus.value;
-  const dueDate = newTaskDueDate.value.trim()
-    ? new Date(newTaskDueDate.value)
-    : null;
+  const dueDate = newTaskDueDate.value;
+  // console.log(dueDate + "line 60");
+    // ? new Date(newTaskDueDate.value)
+    // : null;
 
-  if (!validFormFieldInput(name)) {
+  if (!validFormFieldInput(taskName)) {
     errorMessageTN.innerHTML = "Invalid Name";
     errorMessageTN.style.display = "block";
     newTaskNameInput.focus();
@@ -72,7 +95,29 @@ newTaskForm.addEventListener("submit", (event) => {
     errorMessageASSTO.style.display = "none";
     errorMessageDD.style.display = "none";
 
-    taskManager.addTask(name, description, assignedTo, status, dueDate);
+    // Send validated values to TaskManager
+    if (taskFormMode === "add") {
+      taskManager.addTask(taskName, description, assignedTo, status, dueDate);
+    }
+
+    if (taskFormMode === "edit") {
+      taskManager.editTask(
+        editedTask,
+        taskName,
+        description,
+        assignedTo,
+        status,
+        dueDate
+      );
+      // Switch task form mode to 'add'
+      taskFormMode = "add";
+      // Switch title of the modal
+      taskModalTitle.innerHTML = "Add Task";
+      // Close modal when save button clicked
+      // $("#createTaskModal").modal("toggle");
+    }
+
+    // taskManager.addTask(name, description, assignedTo, status, dueDate);
     taskManager.save();
     taskManager.render();
 
@@ -108,10 +153,40 @@ tasksList.addEventListener("click", (event) => {
     // Save the tasks to localStorage
     taskManager.save();
 
-    // Render the tasks
+    // // Render the tasks
     taskManager.render();
   }
 
+  // Open edit mode when edit-button clicked
+  if (event.target.classList.contains("edit-button")) {
+     // Switch task form mode to 'edit'
+    taskFormMode = "edit";
+    // Switch title of the modal to "edit"
+    taskModalTitle.innerHTML = "Edit Task";
+    updateTask.innerHTML = "Update Task";
+    const parentTask = event.target.parentElement.parentElement;
+//  alert(parentTask);
+    const taskId = Number(parentTask.dataset.taskId);
+    // alert(taskId);
+    editedTask = taskManager.getTaskById(taskId);
+
+    // Fill task form fields with stored values
+    newTaskNameInput.value = editedTask.taskName;
+    newTaskDescription.value = editedTask.description;
+    newTaskAssignedTo.value = editedTask.assignedTo;
+    newStatus.value = editedTask.status;
+    newTaskDueDate.value = editedTask.dueDate;
+    // console.log(newTaskDueDate + "line 179");
+  // }
+
+  // Call taskManager.save() to save task to localStorage
+  taskManager.save();
+
+  // Call taskManager render method to push tasks to html
+  taskManager.render();
+  }
+     
+  
   if (event.target.classList.contains("delete-button")) {
     // Get the parent Task
     const parentTask = event.target.parentElement.parentElement.parentElement;
@@ -137,7 +212,7 @@ function validFormFieldInput(data) {
   }
 }
 
-// // // Select the Edit Task Form
+// // Select the Edit Task Form
 // const editTaskForm = document.querySelector("#editTaskForm");
 
 // // Add an 'onsubmit' event listener
@@ -145,36 +220,36 @@ function validFormFieldInput(data) {
 //   // Prevent default action
 //   event.preventDefault();
 
-//   // Select the inputs for Edit Task Form
-//   const editTaskNameInput = document.querySelector("#editTaskNameInput");
-//   const editTaskDescription = document.querySelector("#editTaskDescription");
-//   const editTaskAssignedTo = document.querySelector("#editTaskAssignedTo");
-//   // const editStatus = document.querySelector("#editStatus");
-//   const editTaskDueDate = document.querySelector("#editTaskDueDate");
-//   const errorMessageEditTN = document.querySelector("#alertMessageEditTN");
-//   const errorMessageEditTDES = document.querySelector("#alertMessageEditTDES");
-//   const errorMessageEditASSTO = document.querySelector(
-//     "#alertMessageEditASSTO"
-//   );
-//   const errorMessageEditDD = document.querySelector("#alertMessageEditDD");
+// Select the inputs for Edit Task Form
+// const editTaskNameInput = document.querySelector("#editTaskNameInput");
+// const editTaskDescription = document.querySelector("#editTaskDescription");
+// const editTaskAssignedTo = document.querySelector("#editTaskAssignedTo");
+// const editStatus = document.querySelector("#editStatus");
+// const editTaskDueDate = document.querySelector("#editTaskDueDate");
+// const errorMessageEditTN = document.querySelector("#alertMessageEditTN");
+// const errorMessageEditTDES = document.querySelector("#alertMessageEditTDES");
+// const errorMessageEditASSTO = document.querySelector(
+//   "#alertMessageEditASSTO"
+// );
+// const errorMessageEditDD = document.querySelector("#alertMessageEditDD");
 
-//   /*
-//       Validation code here
-//   */
+/*
+      Validation code here
+  */
 
-//   // Get the values of the inputs
-//   const editName = editTaskNameInput.value;
-//   const editDescription = editTaskDescription.value;
-//   const editAssignedTo = editTaskAssignedTo.value;
-//   // const editStatus = editStatus.value;
-//   const editDueDate = editTaskDueDate.value;
+// Get the values of the inputs
+// const editName = editTaskNameInput.value;
+// const editDescription = editTaskDescription.value;
+// const editAssignedTo = editTaskAssignedTo.value;
+// const editStatus = editStatus.value;
+// const editDueDate = editTaskDueDate.value;
 
-//   // // Clear the form
-//   // editTaskNameInput.value = "";
-//   // editTaskDescription.value = "";
-//   // editTaskAssignedTo.value = "";
-//   // editStatus.value = "";
-//   // editTaskDueDate.value = "";
+// Clear the form
+// editTaskNameInput.value = "";
+// editTaskDescription.value = "";
+// editTaskAssignedTo.value = "";
+// editStatus.value = "";
+// editTaskDueDate.value = "";
 
 //   if (!validFormFieldInput(editName)) {
 //     errorMessageEditTN.innerHTML = "Invalid Name";
